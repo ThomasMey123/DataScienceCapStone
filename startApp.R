e7a86d9cd5d7c2423a24090c7513978d952a68d5
ThomasMey123 <ThomasMey@yahoo.de>
2016-04-19 05:27:24 +0200

    bugfixes

--- startApp.R
+++ startApp.R
@@ -1,7 +1,7 @@
 setwd("~/git-repos/DataScienceCapStone")
 file.copy("data/small/NGrams/N1GramsKN.csv", "WordPredictionApp/Data",overwrite = TRUE)
-file.copy("data/small/NGrams/N2GramsKN.csv", "WordPredictionApp/Data",overwrite = TRUE)
-file.copy("data/small/NGrams/N3Grams.csv", "WordPredictionApp/Data",overwrite = TRUE)
+file.copy("data/small/NGrams/shrunk_N2GramsKN.csv", "WordPredictionApp/Data/N2GramsKN.csv",overwrite = TRUE)
+file.copy("data/small/NGrams/shrunk_N3Grams.csv", "WordPredictionApp/Data/N3Grams.csv",overwrite = TRUE)
 
 library(shiny)
 runApp("WordPredictionApp")
