e377ea7f4a9ee6dc31791ac2bdc5839ea530d115
Thomas Mey <tmey@munichre.com>
2016-04-23 09:45:23 +0200

    with collapse

--- shrinktable/Program.cs
+++ shrinktable/Program.cs
@@ -25,28 +25,91 @@ namespace shrinkTable
                 numberOfLines = Int32.Parse(args[2]);
             }
 
+            bool collapse = false;
+            if (args.Length == 4 && args[3].Equals("collapse"))
+            {
+                collapse = true;
+            }
+
+
             Console.WriteLine("processing " + filename);
             var reader = new StreamReader(File.OpenRead(filename));
             var writer = new StreamWriter(destfile, false);
-            var doShrink = true;
 
             //read header line
             var line = reader.ReadLine();
             writer.WriteLine(line);
 
 
+            if(collapse == true)
+            {
+                shrinkCollapsed(reader,writer,numberOfLines);
+            }
+            else
+            {
+                shrink(reader, writer, numberOfLines);
+            }
+
+            reader.Close();
+            writer.Close();
+        }
+
+        private static void shrinkCollapsed(StreamReader reader, StreamWriter writer, int numberOfLinesToShrink)
+        {
+            string word = "";
+            var count = 1;
+            var target = "";
+            var t1 = "";
+            var newline = "";
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                t1 = values[0];
+                var t2 = values[1];
+                var freq = values[2];
+
+                if (word.Equals(t1))
+                {
+                    if (count <= numberOfLinesToShrink)
+                    {
+                        target +=  (target.Length == 0 ? "":  " ") + t2.Trim('\"');
+                    }
+                    count++;
+                }
+                else
+                {
+                    if (!word.Equals(""))
+                    {
+                        newline = word + ",\"" + target + "\"";
+                        writer.WriteLine(newline);
+                    }
+                    count = 1;
+                    word = t1;
+                    target = t2.Trim('\"');
+                 }
+            }
+            newline = "\"" + t1 + "\",\"" + target + "\"";
+            writer.WriteLine(newline);
+
+        }
+
+        private static void shrink(StreamReader reader, StreamWriter writer, int numberOfLines)
+        {
             var word = "";
             var count = 1;
             while (!reader.EndOfStream)
             {
-                line = reader.ReadLine();
+                var line = reader.ReadLine();
                 var values = line.Split(',');
 
                 var t1 = values[0];
                 var t2 = values[1];
                 var freq = values[2];
 
-                if(word.Equals(t1))
+                if (word.Equals(t1))
                 {
                     count++;
                 }
@@ -56,22 +119,11 @@ namespace shrinkTable
                     word = t1;
                 }
 
-                if(doShrink)
+                if (count <= numberOfLines)
                 {
-                    if(count<= numberOfLines)
-                    {
-                        writer.WriteLine(line);
-                    }
+                    writer.WriteLine(line);
                 }
-                else
-                {
-                    writer.WriteLine(line + "," + count);
-                }
-
             }
-
-            reader.Close();
-            writer.Close();
         }
     }
 }
